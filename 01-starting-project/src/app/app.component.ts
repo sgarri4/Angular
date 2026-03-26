dcac0dfcca9b8bd95649e7eebd04a823934e8d9f
sgarri4 <sgarri4@xtec.cat>
2026-03-26 20:10:41 +0100

    capitulo 19

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from './header/header.component';
+//Decorador
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
