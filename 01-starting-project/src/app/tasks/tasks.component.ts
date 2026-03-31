dc68aaa7fe73396e8e88c662a026371079982cc1
sgarri4 <sgarri4@ilg.cat>
2026-03-31 21:49:26 +0200

    añadido task

---
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({required: true}) name?: string;
+   @Input({required: true}) userId!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular ',
+      dueDate: '2026-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular ',
+      dueDate: '2026-12-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular ',
+      dueDate: '2026-12-31'
+    },
+    {
+      id: 't4',
+      userId: 'u4',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular ',
+      dueDate: '2026-12-31'
+    }
+  ]
+
+  get selectedUserTasks(){
+    return this.tasks.filter((task)=> task.userId === this.userId);
+  }
+}
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,16 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
+// type User = {
+//     id: string;
+//     avatar: string;
+//     name: string;
+// }
+
+interface User {
+    id: string;
+    avatar: string;
+    name: string;
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,13 +19,19 @@ import { Component, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  @Input({required:true}) user!: User;
+  @Output() select = new EventEmitter();
+  //select = output<string>();
 
   get imagePath(){
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
-  onSelectUser(){}
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }
 }
 
 
